b59e8df51a5b594b62729d5d8c9ad614c2527d76
viccjiang <binbinbb520@gmail.com>
2021-06-08 17:24:08 +0800

    20210608_01 助教建議修改

--- js/all.js
+++ js/all.js
@@ -12,14 +12,13 @@ const app = {
   },
   // 取得商品列表資料方法
   getData() {
-    axios
-      .get(`${url}/api/${path}/products`)
+    axios.get(`${url}/api/${path}/admin/products`)
       .then((res) => {
         // console.log(res);
-        if (res.data.success) { //成功的話
+        if (res.data.success) { //成功的話，把資料存起來
           this.data.products = res.data.products;//把產品資料存起來
           // console.log(this.data.products);
-          this.render();//渲染產品列表，要先寫fn
+          this.render(); //觸發31行的render 渲染產品列表，要先寫fn 
         }
       })
       .catch((error) => {
@@ -28,7 +27,6 @@ const app = {
   },
 
   // 渲染產品列表
-
   render() {
     //要把畫面渲染在productList，把DOM取出來#productList
     const productsListDom = document.querySelector('#productList');
@@ -44,13 +42,12 @@ const app = {
         <td width="120">${item.origin_price}</td>
         <td width="120">${item.price}</td>
         <td width="100"><div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id=" ${item.id}" ${item.is_enabled ? 'checked' : ''
-        } data-action="complete" data-id="${item.id}">
-        <label class="form-check-label" for="is_enabled">${item.is_enabled ? '啟用' : '未啟用'
-        }</label>
-      </div></td>
-        <td width="120"><button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove" data-id="${item.id
-        }"> 刪除 </button></td>
+        <input class="form-check-input" type="checkbox" id=" ${item.id}" ${item.is_enabled ? 'checked' : ''} data-action="complete" data-id="${item.id}">
+        <label class="form-check-label" for="is_enabled">${item.is_enabled ? '啟用' : '未啟用'}</label>
+        </div></td>
+        <td width="120">
+          <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove" data-id="${item.id}"> 刪除 </button>
+        </td>
       </tr>
       `;
     });
@@ -58,51 +55,59 @@ const app = {
     productsListDom.innerHTML = str;
     productLen.textContent = `${this.data.products.length}`;
 
-
     // 刪除按鈕監聽
     const deleteBtns = document.querySelectorAll('.deleteBtn');
     // console.log(deleteBtns);
     deleteBtns.forEach((btn) => {
-      btn.addEventListener('click', this.deleteProduct);
+      btn.addEventListener('click', this.deleteProduct); //為所有按鈕加上監聽 ，用 deleteProduct 函式觸發事件
     });
   },
 
-
-  // 刪除產品方法
+  // 刪除產品方法，觸發刪除事件
   deleteProduct(evt) {
     // console.log('觸發刪除事件');
     // 監聽會觸發事件物件
     // dataset 可以找到 id
     const id = evt.target.dataset.id; //DOM元素操作
-    // console.log(evt, id);
+    // console.log('deleteProduct',evt, id);
     axios.delete(`${url}/api/${path}/admin/product/${id}`)
       .then((res) => {
         // console.log(res);
         // 因為是在deleteBtns DOM元素內，this指向會有問題，所以不能寫this.render()
-        app.getData(); //用getData觸發render
+        app.getData(); //用 getData 觸發 render
         alert('刪除成功!');
       })
       .catch((error) => {
         console.log(error);
       });
   },
+
   // 初始化
   init() {
-    // 取出cookie
-    const token = document.cookie.replace(
-      /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
-      '$1'
-    );
+    // 取出cookie 記得hexToken要寫進去 
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, '$1');
     console.log(token);
     //把 token 加到 axios 之後每次發送API時 才能再把 Token 帶出去
     axios.defaults.headers.common['Authorization'] = token;
     // console.log(token);
-    //直接發送驗證API檢查，檢查用戶是否登入
+    // 針對同一個站點
+
+    //直接發送驗證 API檢查，檢查用戶是否登入
     axios.post(`${url}/api/user/check`)
       .then(res => {
         console.log(res);
+        if (res.data.success) {
+          console.log('登入成功');
+        } else {
+          alert('登入失敗，請重新輸入帳號、密碼');
+          window.location = 'login.html'; //未登入的話跳轉到 login
+        }
       })
-    // 針對同一個站點
+      .catch((error) => {
+        console.log(error);
+
+      })
+
 
     // 取得商品列表資料
     this.getData();
--- js/login.js
+++ js/login.js
@@ -4,18 +4,20 @@ const url = 'https://vue3-course-api.hexschool.io';
 const path = 'jiangsvue3';
 
 // 登入頁 取出dom元素
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
-const loginBtn = document.querySelector('#login');
+const usernameInput = document.querySelector('#username'); // 使用者名稱
+const passwordInput = document.querySelector('#password'); // 使用者密碼
+const loginBtn = document.querySelector('#login'); // 登入按鈕
 
 
 // 登入按鈕監聽  觸發按鈕監聽事件   
 loginBtn.addEventListener('click', login);
 
 // 登入方法
+// 取得 Token
+// 將 Token 存到 Cookie
 function login() {
   // console.log('成功觸發');
-  //這裡是個雷，要把帳密放到fn裡才會取到值，不能放到fn外
+  //這裡是個雷，要把帳密放到fn裡才會取到值，不能放到fn外 (函式觸發後才能取到值)
   const username = usernameInput.value;
   const password = passwordInput.value;
   const loginData = {
@@ -38,6 +40,7 @@ function login() {
         console.log(token, expired);
         // 儲存cookie
         // 儲存 cookie ( 這段程式碼來自 MDN ) 代入token ＆ 有效日期
+        // hexToken 要記得寫進去
         document.cookie = `hexToken=${token};expired=${new Date(expired)}; path=/`;
         window.location = 'index.html'; //成功的話跳轉到 index
       } else {
